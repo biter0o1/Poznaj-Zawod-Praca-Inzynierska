ef49333b72f769f203e3b02dbb947d70d0e550be
biter0o1 <biter0o1@o2.pl>
2022-10-25 23:47:38 +0200

    Fix some issues with layout

--- src/Controller/ExampleJsonController.php
+++ src/Controller/ExampleJsonController.php
@@ -17,12 +17,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExampleJsonController extends AbstractFOSRestController
 {
     #[Route('/', name: 'app_exampleJson_index', methods: 'GET')]
-    public function testJson(ExampleRepository $exampleRepository, Request $request): View
+    public function testJson(ExampleRepository $exampleRepository, Request $request): JsonResponse
     {
         $query = $request->get('query');
         $workers = $exampleRepository->findAll();
+        $serializedEntity = $this->container->get('serializer')->serialize($workers, 'json');
+
+//        return $this->view(['data' => $workers, 'count' => count($workers)], Response::HTTP_OK);
+        return new JsonResponse(array('name' => $serializedEntity));
+    }
+
+    #[Route('/test', name: 'app_exampleJson_test', methods: 'GET')]
+    public function testJsontest(ExampleRepository $exampleRepository, Request $request): View
+    {
+        $query = $request->get('query');
+        $workers = $exampleRepository->findAll();
+        $serializedEntity = $this->container->get('serializer')->serialize($workers, 'json');
 
         return $this->view(['data' => $workers, 'count' => count($workers)], Response::HTTP_OK);
+//        return new JsonResponse(array('name' => $serializedEntity));
     }
 
 }
